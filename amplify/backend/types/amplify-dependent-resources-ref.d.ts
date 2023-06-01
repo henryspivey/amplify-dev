9ba04e6b9c43b91912d7094126bc55a5ce3b6f7a
Henry Spivey <henry.spivey@changehealthcare.com>
2023-05-31 20:57:55 -0700

    added api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifydev": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
